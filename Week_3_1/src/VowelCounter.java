c3cbba912b37e5027cb50bb1e4fe4379521585e2
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-07-27 16:59:09 -0600

    second commit

--- Week_3_1/src/VowelCounter.java
+++ Week_3_1/src/VowelCounter.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.String;
+import java.lang.Character;
 
 public class VowelCounter {
 
@@ -13,25 +14,22 @@ public class VowelCounter {
 		// have a separate counter for each vowel
 		// find the non-vowel characters in the string
 		// count and print the number of non-vowel characters
-		// 
+		//
 
 		Scanner scan = new Scanner(System.in);
-		String string;
-		int max, aChar = 0;
-		
+		String string, aString;
+		int max, aCharA;
+		char a = 'a', e = 'e', i = 'i', o = 'o', u = 'u';
 		System.out.println("Type something with vowels in it: ");
 		string = scan.nextLine();
 		System.out.println("This is what you typed: " + string);
-
 		max = string.length();
-		for (aChar = 0 ; aChar < max ; aChar++) { 
-			System.out.println(string.indexOf('a'));
-			System.out.println(string.lastIndexOf('a'));
-	}
-	}
-}		
 
-	
-		
-		
-	
+		for (int aChar = 0; aChar < max; aChar++)
+			aCharA = string.indexOf(aChar);
+			System.out.println(aCharA);
+			// System.out.println(string.lastIndexOf('a'));
+
+		}
+}
+}
