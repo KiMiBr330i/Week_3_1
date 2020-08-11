8c1131bb498e4fa4e32b208427ea8148fbb3c02d
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-10 18:49:32 -0600

    commit 08-10-2020 1848hrs - corrections to code. Simplified the code responsible for counting non-vowel characters.

--- Week_3_1/src/VowelCounter.java
+++ Week_3_1/src/VowelCounter.java
@@ -1,8 +1,9 @@
 //******************************************************************************************
 // Lab 3 - Exercise 3  Kip Brower 07/27/20 CPSC-50100
 //
-// VowelCounter.java - a program to count each type of vowel and all consonants in a user-
-// supplied string.
+// VowelCounter.java - a program to count each type of vowel and all non-vowel characters
+//(including spaces) in a user-supplied string.
+//
 //******************************************************************************************
 
 import java.util.Scanner;
@@ -14,17 +15,15 @@ public class VowelCounter {
 
 		Scanner scan = new Scanner(System.in);
 		String string;
-		char a = 'a', e = 'e', i = 'i', o = 'o', u = 'u', f = 'f', b = 'b', c = 'c', d = 'd', g = 'g', h = 'h', j = 'j',
-				k = 'k', l = 'l', m = 'm', n = 'n', p = 'p', q = 'q', r = 'r', s = 's', t = 't', v = 'v', w = 'w',
-				x = 'x', y = 'y', z = 'z';
+		char a = 'a', e = 'e', i = 'i', o = 'o', u = 'u';
 
 		int max, acount = 0, ecount = 0, ocount = 0, icount = 0, ucount = 0, nvCount = 0;
 		System.out.print("Type a phrase and this program will tell you ");
-		System.out.println("how many of each type of vowel and number of consonants are in it: ");
+		System.out.println("how many of each type of vowel and number of non-vowel characters are in it: ");
 		string = scan.nextLine();
 		max = string.length();
 
-		for (int index = 0; index < max; index++) { // loop for scanning through the User supplied string
+		for (int index = 0; index < max; index++) { // loop for scanning through the user-supplied string
 
 			// ----------------------------------------------------------------------------------------------------------
 			// conditional statements identifying and counting vowels
@@ -44,88 +43,36 @@ public class VowelCounter {
 
 			} else if (u == string.charAt((index))) {
 				ucount++;
-			}
 
-			// ----------------------------------------------------------------------------------------------------------
-			// conditional statements identifying and counting consonants
-			// ----------------------------------------------------------------------------------------------------------
+				// ----------------------------------------------------------------------------------------------------------
+				// conditional statements identifying, counting non-vowel characters
+				// ----------------------------------------------------------------------------------------------------------
 
-			if (b == string.charAt((index))) {
+			} else if (a != string.charAt((index))) {
 				nvCount++;
 
-			} else if (c == string.charAt((index))) {
+			} else if (e != string.charAt((index))) {
 				nvCount++;
 
-			} else if (d == string.charAt((index))) {
+			} else if (i != string.charAt((index))) {
 				nvCount++;
 
-			} else if (f == string.charAt((index))) {
+			} else if (o != string.charAt((index))) {
 				nvCount++;
 
-			} else if (g == string.charAt((index))) {
-				nvCount++;
-
-			} else if (h == string.charAt((index))) {
-				nvCount++;
-
-			} else if (j == string.charAt((index))) {
-				nvCount++;
-
-			} else if (k == string.charAt((index))) {
-				nvCount++;
-
-			} else if (l == string.charAt((index))) {
-				nvCount++;
-
-			} else if (m == string.charAt((index))) {
-				nvCount++;
-
-			} else if (n == string.charAt((index))) {
-				nvCount++;
-
-			} else if (p == string.charAt((index))) {
-				nvCount++;
-
-			} else if (q == string.charAt((index))) {
-				nvCount++;
-
-			} else if (r == string.charAt((index))) {
-				nvCount++;
-
-			} else if (s == string.charAt((index))) {
-				nvCount++;
-
-			} else if (t == string.charAt((index))) {
-				nvCount++;
-
-			} else if (v == string.charAt((index))) {
-				nvCount++;
-
-			} else if (w == string.charAt((index))) {
-				nvCount++;
-
-			} else if (x == string.charAt((index))) {
-				nvCount++;
-
-			} else if (y == string.charAt((index))) {
-				nvCount++;
-
-			} else if (z == string.charAt((index))) {
+			} else if (u != string.charAt((index))) {
 				nvCount++;
 
 			}
 
 		}
-		// ----------------------------------------------------------------------------------------------------------
-		// conditional statements identifying and counting consonants
-		// ----------------------------------------------------------------------------------------------------------
 
-		System.out.println("There are " + acount + " lower case a letters");
-		System.out.println("There are " + ecount + " lower case e letters");
-		System.out.println("There are " + icount + " lower case i letters");
-		System.out.println("There are " + ocount + " lower case o letters");
-		System.out.println("There are " + ucount + " lower case u letters");
-		System.out.println("There are " + nvCount + " non-vowels");
+		System.out.println("There are " + acount + " lower case a letters.");
+		System.out.println("There are " + ecount + " lower case e letters.");
+		System.out.println("There are " + icount + " lower case i letters.");
+		System.out.println("There are " + ocount + " lower case o letters.");
+		System.out.println("There are " + ucount + " lower case u letters.");
+		System.out.println("There are " + nvCount + " non-vowel characters, including spaces.");
 
 		scan.close();
 	}
